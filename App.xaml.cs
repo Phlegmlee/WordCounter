9cdd25e81090f0ed0a74e5c75b5a0983a02a0ccc
Phlegmlee <152333980+Phlegmlee@users.noreply.github.com>
2025-08-04 18:54:27 -0600

    MAJOR - Create MAUI Project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace WordCounter
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WordCounter
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
