b8060dc111e22e57cf3ff836f00a92dd80a2e9cf
Phlegmlee <152333980+Phlegmlee@users.noreply.github.com>
2025-08-05 20:44:17 -0600

    MAUI Conversion - 002
    Created LogItem class
    
    Wrote the xaml code for a list view that has bindings to the LogItem class.
    
    Wrote File creation logic based on file existence.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,30 +1,66 @@
-﻿namespace WordCounter
+﻿using System.IO;
+using WordCounter.Models;
+
+
+namespace WordCounter
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
+        const string log_file = "WordCounter_log.txt";
+        const string wc_file = "WordCounter_wc.txt";
+        string log_path = System.IO.Path.Combine(FileSystem.Current.AppDataDirectory, log_file);
+        string wc_path = System.IO.Path.Combine(FileSystem.Current.AppDataDirectory, wc_file);
+        long previous_word_total = 0;
+
 
         public MainPage()
         {
             InitializeComponent();
+            Loaded += MainPage_Loaded;
+
+            var logItems = new List<LogItem>
+            {
+                new LogItem { WordCount = "9999999", WordDiff = "9999999", DateValue = "2025-08-05 19:30", NotesValue = ""},
+                new LogItem { WordCount = "959", WordDiff = "699", DateValue = "2025-08-05 19:30", NotesValue = ""}
+            };
+
+            listView.ItemsSource = logItems;
+        }
+
+        private void MainPage_Loaded(object? sender, EventArgs e)
+        {
+            if (!File.Exists(log_path))
+            {
+                File.Create(log_path).Close();
+            }
+            else if (File.Exists(log_path))
+            {      
+                var log = File.ReadAllLines(log_path);
+                foreach (string s in log)
+                {
+                    // Parse file, insert data into LogItem somehow????????
+                }
+            }
+
+            if (!File.Exists(wc_path))
+            {
+                File.Create(wc_path).Close();
+                File.WriteAllText(wc_path, "0");
+            }
+            else if (File.Exists(wc_path))
+            {
+                string wordCount = File.ReadAllText(wc_path);
+                previous_word_total = long.Parse(wordCount);
+
+                // Populate Word Count Boxes?
+            }
         }
 
         private void OnSubmitClicked(object sender, EventArgs e)
         {
-            count++;
 
-            if (count == 1)
-                SubmitBtn.Text = $"Clicked {count} time";
-            else
-                SubmitBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(SubmitBtn.Text);
         }
 
-        public class Log
-        {
-            public string Words = "MEEP MORP";
-        }
     }
 
 }
---
+++ Models/LogItem.cs
@@ -0,0 +1,10 @@
+﻿namespace WordCounter.Models
+{
+    internal class LogItem
+    {
+        public string WordCount { get; set; }
+        public string WordDiff { get; set; }
+        public string DateValue { get; set; }
+        public string NotesValue { get; set; }
+    }
+}
